8403ce379dbeb392eabbb597eb8a4a4ede04541f
Randy Mackay <rmackay9@yahoo.com>
2017-04-13 19:40:10 +0900

    mavros_extras: fix vision-position-delta plugin

--- mavros_extras/src/plugins/vision_position_delta.cpp
+++ mavros_extras/src/plugins/vision_position_delta.cpp
@@ -55,33 +55,33 @@ private:
 
 	/* send vision_position_delta message to ardupilot */
 	void vision_position_delta_send(
-            uint64_t time_usec,                 // Timestamp (microseconds, synced to UNIX time or since system boot)
-            uint64_t time_delta_usec,           // Time in microseconds since the last reported camera frame
+			uint64_t time_usec,                 // Timestamp (microseconds, synced to UNIX time or since system boot)
+			uint64_t time_delta_usec,           // Time in microseconds since the last reported camera frame
 			Eigen::Vector3d &angle_delta,       // Defines a rotation vector in body frame that rotates the vehicle from the previous to the current orientation
-            Eigen::Vector3d &position_delta,    // Change in position in meters from previous to current frame rotated into body frame (0=forward, 1=right, 2=down)
+        	Eigen::Vector3d &position_delta,    // Change in position in meters from previous to current frame rotated into body frame (0=forward, 1=right, 2=down)
 			float confidence)                   // normalised confidence value from 0 to 100
 	{
 		mavlink::ardupilotmega::msg::VISION_POSITION_DELTA vpd{};
 
 		vpd.time_usec = time_usec;
-        vpd.time_delta_usec = time_delta_usec;
-        vpd.angle_delta = angle_delta;
-        vpd.position_delta = position_delta;
-        vpd.confidence = confidence;
+		vpd.time_delta_usec = time_delta_usec;
+		vpd.angle_delta = angle_delta;
+		vpd.position_delta = position_delta;
+		vpd.confidence = confidence;
 		UAS_FCU(m_uas)->send_message_ignore_drop(vpd);
 	}
 
 	/* callback when odometry is updated, triggers send to ardupilot */
-	void odom_cb(const geometry_msgs::PoseStamped::ConstPtr &req)
+	void odom_cb(const nav_msgs::Odometry::ConstPtr &req)
 	{
-        // get time of this message
-        // calculate time_delta_usec using last_update (sanity check time difference)
-        // store time of this message in last_update
-        uint64_t time_usec = 0;
-        uint64_t time_delta_usec = 0;
+		// get time of this message
+		// calculate time_delta_usec using last_update (sanity check time difference)
+		// store time of this message in last_update
+		uint64_t time_usec = 0;
+		uint64_t time_delta_usec = 0;
 		Eigen::Vector3d angle_delta;
-        Eigen::Vector3d position_delta;
-        float confidence = 0;
+		Eigen::Vector3d position_delta;
+		float confidence = 0;
 		vision_position_delta_send(time_usec, time_delta_usec, angle_delta, position_delta, confidence);
 	}
 };
@@ -89,4 +89,4 @@ private:
 }	// namespace mavros
 
 #include <pluginlib/class_list_macros.h>
-PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::VisionPositionDelta, mavros::plugin::PluginBase)
+PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::VisionPositionDeltaPlugin, mavros::plugin::PluginBase)
